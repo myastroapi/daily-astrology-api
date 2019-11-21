fd350dc31e8a7b23ed686e7d624fed4fb3292ad0
myastroapi <57897555+myastroapi@users.noreply.github.com>
2019-11-21 23:03:28 +0530

    Add files via upload

---
+++ Myastro.php
@@ -0,0 +1,43 @@
+<?php
+class Myastro extends Myastroapi
+{
+private $api;
+private $url;
+private $version;
+function __construct()
+{
+$this->api=$this->_api;
+$this->url=$_url;
+}
+protected function getapi()
+{
+$init=curl_init();
+curl_setopt($init,CURLOPT_URL,trim($this->url));
+curl_setopt($init,CURLOPT_POST,TRUE);
+curl_setopt($init,CURLOPT_RETURNTRANSFER,TRUE);
+curl_setopt($init,CURLOPT_POSTFIELDS,["api"=>$this->api,"version"=>$this->version]);
+$data=curl_exec();
+$getinfo=curl_getinfo($data);
+$status=$getinfo["http_code"];
+if($status==200)
+{
+return [true,$data];
+}
+else
+{
+return [false,$status];
+}
+}
+function getdaily()
+{
+$data=$this->getapi();
+if($data[0])
+{
+return json_decode($data,true);
+}
+else
+{
+return $data[1];
+}
+}
+}
