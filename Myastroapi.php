70820159c5644f34e2152942df4a46bc658bfd56
myastroapi <57897555+myastroapi@users.noreply.github.com>
2019-11-21 23:13:31 +0530

    Create Myastroapi.php

---
+++ Myastroapi.php
@@ -0,0 +1,17 @@
+<?php
+class Myastroapi
+{
+private $_api;
+private $_version;
+private $_url;
+function __construct()
+{
+//add your api key here
+
+$this->_api="";
+
+//no need to change below it is recommanded
+$this->_version="1.0";
+$this->_url="http://service.kundalimilan.online/api/version.php";
+}
+}
