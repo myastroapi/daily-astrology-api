c2526c01b6858e67092dc95783272c2e3fba81d1
myastro <57897555+myastroapi@users.noreply.github.com>
2019-12-06 06:03:43 +0530

    Changes Api Key STORE
    Now you need to store your api key in text format in __api.txt file.

--- Myastroapi.php
+++ Myastroapi.php
@@ -7,11 +7,18 @@ private $_url;
 function __construct()
 {
 //add your api key here
-
-$this->_api="";
-
+$key=$this->initapikeyfromfile();
+$this->_api=$key;
 //no need to change below it is recommanded
 $this->_version="1.0";
 $this->_url="http://service.kundalimilan.online/api/version.php";
+}
+private function initapikeyfromfile()
+{
+$fp=fopen(__DIR__."/__api.txt","W");
+$api=trim(fread($fp,1024));
+//you should need to check for api key character  
+fclose($fp); 
+return $api;  
 }
 }
